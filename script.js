0cd29aceca762f2322c1e7156d6654945eab847a
Prerana Dhayfule <132763290+08prerna@users.noreply.github.com>
2024-02-09 14:46:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+let randNumber=Math.floor(Math.random()*100)+1;
+
+const submit = document.querySelector("#guessSubmit");
+const userInput = document.querySelector(".guessField");
+const prevGuess = document.querySelector(".prevGuess");
+const remGuess = document.querySelector(".remGuess");
+const start = document.querySelector(".result-container");
+const estimation = document.querySelector(".estimation");
+
+// const p=document.createElement("p");
+const button=document.createElement("button");
+
+let previousGuess=[];
+let noGuess=1;
+let startGame=true;
+
+if(startGame){
+    submit.addEventListener("click",function(e) {
+        e.preventDefault();
+        const guess=parseInt(userInput.value);
+        console.log(guess);
+        validateGuess(guess);
+    })
+}
+
+function validateGuess(guess) {
+    if(isNaN(guess)){
+        alert("Please enter a valid number");
+    }
+    else if(guess<=0){
+        alert("Please enter a value greater than 0");
+    }
+    else if(guess>100){
+        alert("Please enter a value less than 100");
+    }
+    else{
+        previousGuess.push(guess);
+        if (noGuess===11){
+            displayGuess(guess);
+            estimation.innerHTML=`<h1>Game Over! Number was ${randNumber}</h1>`;
+            endGame();
+        }
+        else{
+            displayGuess(guess);
+            checkGuess(guess);
+        }
+    }
+}
+
+function displayGuess(guess) {
+    userInput.value="";
+    prevGuess.innerHTML+=` ${guess}`;
+    noGuess++;
+    remGuess.innerHTML=`${11-noGuess}`;
+}
+
+function checkGuess(guess) {
+    if(guess===randNumber){
+        estimation.innerHTML = `<h1 class="fs-3 text-info-emphasis">You guessed correctly!</h1>`;
+        endGame();
+    }
+    else if(guess<randNumber){
+        estimation.innerHTML = `<h1 class="fs-3 text-info-emphasis">Too Low! Try again!</h1>`
+    }
+    else{
+        estimation.innerHTML = `<h1 class="fs-3 text-info-emphasis">Too High! Try again!</h1>`
+    }
+}
+
+function endGame() {
+    userInput.value="";
+    userInput.setAttribute("disabled","");
+    // p.classList.add("newStart");
+    button.classList.add("w-100", "py-2", "mb-2", "btn", "btn-outline-primary", "rounded-3", "newStart")
+    // p.innerHTML=`<h1>Start new Game</h1>`;
+    button.innerHTML='Start New Game';
+    // start.appendChild(p);
+    start.appendChild(button);
+    startGame=false;
+    newGame();
+}
+
+function newGame() {
+    const newStart=document.querySelector(".newStart");
+    newStart.addEventListener("click",function () {
+        randNumber = Math.floor(Math.random() * 100) + 1;
+        previousGuess = [];
+        noGuess = 1;
+        startGame = true;
+        prevGuess.innerHTML="";
+        remGuess.innerHTML=`${11-noGuess}`;
+        estimation.innerHTML="";
+        userInput.removeAttribute("disabled");
+        // start.removeChild(p);
+        start.removeChild(button);
+    })
+}
+console.log(randNumber);
